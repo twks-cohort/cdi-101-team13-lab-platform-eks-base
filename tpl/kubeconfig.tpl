6019cf6e863789aff3eb778a9c643dbbc4887a8b
kaustubhsonar <sonarkaustubh@gmail.com>
2023-08-24 17:35:33 +0530

    #0001 | Kaustubh | Add: repo copied and some modifications

---
+++ tpl/kubeconfig.tpl
@@ -0,0 +1,1 @@
+{{ op://cohorts/blue-platform-$ENV/kubeconfig }}
---
+++ variables.tf
@@ -0,0 +1,35 @@
+variable "aws_region" {}
+variable "aws_account_id" {
+  sensitive = true
+}
+
+variable "aws_assume_role" {
+  sensitive = true
+}
+
+variable "cluster_name" {}
+variable "cluster_version" {}
+variable "cluster_enabled_log_types" {}
+variable "cluster_log_retention" {}
+variable "vpc_cni_version" {}
+variable "coredns_version" {}
+variable "kube_proxy_version" {}
+variable "aws_ebs_csi_version" {}
+variable "alert_channel" {}
+
+variable "default_node_group_name" {}
+variable "default_node_group_ami_type" {}
+variable "default_node_group_platform" {}
+variable "default_node_group_min_size" {}
+variable "default_node_group_max_size" {}
+variable "default_node_group_desired_size" {}
+variable "default_node_group_disk_size" {}
+variable "default_node_group_capacity_type" {}
+variable "default_node_group_instance_types" {}
+
+variable "oidc_client_id" {
+  sensitive = true
+}
+variable "oidc_groups_claim" {}
+variable "oidc_identity_provider_config_name" {}
+variable "oidc_issuer_url" {}
